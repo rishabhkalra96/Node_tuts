abd8bef1bd9aad8c2a3693e8917358813ecf1fe1
Rishabh Kalra <rishabhkalra96@gmail.com>
2018-11-11 10:24:34 +0530

    Added Ping Route and handler to check if the app is alive

--- Node_tuts/index.js
+++ Node_tuts/index.js
@@ -185,6 +185,11 @@ unifiedServer = function(req,res){
 
 var handlers = {};
 
+//a ping route handler so that we can check if the server is alive or dead
+handlers.ping = function(data, callback){
+    callback(200);
+};
+
 handlers.sampler = function(data, callback){
     //console.log("details of the req", data, "\n")
     callback(406, {'name': "Sampler Handler"});
@@ -199,4 +204,5 @@ handlers.notFound = function(data, callback){
 //defining a router
 var router = {
     'sampler' : handlers.sampler,
+    'ping': handlers.ping
 };
